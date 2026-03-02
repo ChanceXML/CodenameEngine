2b1515c5d07521fe979edae0f5c726aa5ae258ff
ChanceXML <joseguest1337@gmail.com>
2026-03-02 16:04:56 -0700

    Update Main.hx

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -53,7 +53,6 @@ class Main extends Sprite
 
 	public static function preInit() {	
 		CrashLogger.init();
-		funkin.backend.utils.AndroidPermissions.request();
 		funkin.backend.utils.NativeAPI.registerAsDPICompatible();
 		funkin.backend.system.CommandLineHandler.parseCommandLine(Sys.args());
 		funkin.backend.system.Main.fixWorkingDirectory();
