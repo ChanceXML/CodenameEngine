c930e54118b797ca8af0b7e7d7c3c2fb0792ad80
ChanceXML <joseguest1337@gmail.com>
2026-03-02 15:49:35 -0700

    Update AndroidPermissions.hx

--- source/funkin/backend/utils/AndroidPermissions.hx
+++ source/funkin/backend/utils/AndroidPermissions.hx
@@ -4,32 +4,26 @@ package funkin.backend.utils;
 import android.Permissions;
 #end
 
-/**
- * Utility class to handle Android storage permissions.
- */
 class AndroidPermissions {
     public static function request():Void {
         #if android
-        var requestedPermissions:Array<String> = [
+        var permissions:Array<String> = [
             "android.permission.READ_EXTERNAL_STORAGE",
             "android.permission.WRITE_EXTERNAL_STORAGE"
         ];
+
+        var toRequest:Array<String> = [];
+        var granted = Permissions.getGrantedPermissions();
         
-        var ungrantedPermissions:Array<String> = new Array<String>();
-        for (permission in requestedPermissions) {
-            if (!Permissions.getGrantedPermissions().contains(permission)) {
-                ungrantedPermissions.push(permission);
+        for (p in permissions) {
+            if (!granted.contains(p)) {
+                toRequest.push(p);
             }
         }
-        
-        if (ungrantedPermissions.length > 0) {
-            Permissions.requestPermissions(ungrantedPermissions);
-            trace("Requesting Android storage permissions...");
-        } else {
-            trace("Storage permissions are already granted.");
+
+        if (toRequest.length > 0) {
+            Permissions.requestPermissions(toRequest);
         }
-        #else
-        trace("Skipping Android permission request: Target is not Android.");
         #end
     }
 }
