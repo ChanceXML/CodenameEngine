972dcdec28703d30ab9453af21356fcadad47c9d
ChanceXML <joseguest1337@gmail.com>
2026-03-02 18:19:40 -0700

    Create MobileUtil.hx

---
+++ source/funkin/mobile/utils/MobileUtil.hx
@@ -0,0 +1,40 @@
+package funkin.mobile.utils;
+
+#if android
+import extension.androidtools.os.Build.VERSION;
+import extension.androidtools.os.Environment;
+import extension.androidtools.Permissions;
+import extension.androidtools.Settings;
+#end
+
+import lime.system.System;
+import openfl.Assets;
+import haxe.io.Bytes;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
+
+using StringTools;
+
+class MobileUtil {
+
+    public static function getPermissions():Void {
+        #if android
+        try {
+            if (VERSION.SDK_INT >= 30) {
+                if (!Environment.isExternalStorageManager()) {
+                    Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+                }
+            } else {
+                Permissions.requestPermissions([
+                    'READ_EXTERNAL_STORAGE',
+                    'WRITE_EXTERNAL_STORAGE'
+                ]);
+            }
+        } catch (e:Dynamic) {
+            trace('Permission request error: $e');
+        }
+        #end
+    }
